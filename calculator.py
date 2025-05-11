ff08a70bedc78b16f11d08c5c26a55d91e82c28c
khlabib <193776169+khlabib@users.noreply.github.com>
2025-05-11 17:33:48 +0600

    Simple calculator added

---
+++ calculator.py
@@ -0,0 +1,7 @@
+def main():
+    a=float(input('what is the first number ? '))
+    b=float(input('what is the second numebr ? '))
+    print('the sum of a and b is',round(sum(a,b),4))
+def sum(a,b):
+    return a+b
+main()
